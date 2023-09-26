5ea08b47a83125189e05c7ad41b5c6ac6bd29b21
Afrodite <106694662+vinicinho30@users.noreply.github.com>
2023-09-26 13:23:38 -0300

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,1 +1,11 @@
-
+//creating an array and passing the number, question, options, and answers
+let question = [
+  {
+    numb:1,
+    question: "Quem foi a única pessoa na história a receber o Prêmio Nobel em áreas científicas diferentes?",
+    answer: "Marie Ciriee",
+    options: [
+      "Albert Eistein",
+      "Marie Curiee",
+      "Stephen Hawking",
+      "Linus Pauling"
